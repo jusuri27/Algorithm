92250a3d2bd71d7f868ef05fec03ef9b8dbff248
jusuri27 <jusuri27@gmail.com>
2026-01-03 16:20:57 +0900

    [level 3] Title: 여행경로, Time: 93.84 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,30 +2,23 @@ import java.util.*;
 
 class Solution {
     static boolean[] visited;
-    static ArrayList<String> allRoute;
-    
+    static ArrayList<String> allRoute = new ArrayList<>();
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
         visited = new boolean[tickets.length];
-        allRoute = new ArrayList<>();
-        int cnt = 0;
-        
-        dfs("ICN" , "ICN", tickets, cnt);
-        
+        String[] answer = {};
+        dfs("ICN", "ICN", tickets, 0);
         Collections.sort(allRoute);
         answer = allRoute.get(0).split(" ");
         return answer;
     }
-    
     public void dfs(String start, String route, String[][] tickets, int cnt) {
         if(cnt == tickets.length) {
             allRoute.add(route);
         }
-        
         for(int i=0; i<tickets.length; i++) {
-            if(start.equals(tickets[i][0]) && !visited[i]) {
+            if(!visited[i] && start.equals(tickets[i][0])) {
                 visited[i] = true;
-                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt + 1);
+                dfs(tickets[i][1], route + " " + tickets[i][1], tickets, cnt+1);
                 visited[i] = false;
             }
         }
