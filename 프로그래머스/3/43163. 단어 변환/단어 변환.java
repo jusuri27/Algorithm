3b7c7fcd39f9c39b5a07d3e94555b3173697d62d
jusuri27 <jusuri27@gmail.com>
2026-01-03 15:18:50 +0900

    [level 3] Title: 단어 변환, Time: 0.45 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,40 +1,31 @@
+import java.util.*;
+
 class Solution {
-    static int answer;
-    static boolean[] visited;
+    static int answer = 0;
     public int solution(String begin, String target, String[] words) {
-        answer = Integer.MAX_VALUE;
-        visited = new boolean[words.length];
-        
-        dfs(begin, target, words, 0);
-        
-        if(answer == Integer.MAX_VALUE) {
-            answer = 0;
-        }
-        
+        int cnt = 0;
+        boolean[] visited = new boolean[words.length];
+        dfs(begin, target, words, cnt, visited);
+        System.out.println(answer);
         return answer;
     }
-    
-    public void dfs(String begin, String target, String[] words, int cnt) {
+    public void dfs(String begin, String target, String[] words, int cnt, boolean[] visited) {
+        
         if(begin.equals(target)) {
-            answer = Math.min(answer, cnt);
+            answer = cnt;
             return;
         }
-        
         for(int i=0; i<words.length; i++) {
             int k = 0;
             
-            if(visited[i]) {
-               continue; 
-            }
-            for(int j=0; j<words[i].length(); j++) {
+            for(int j=0; j<begin.length(); j++) {
                 if(begin.charAt(j) == words[i].charAt(j)) {
                     k++;
                 }
             }
-            
-            if(k >= begin.length() - 1) {
+            if(!visited[i] && k >= begin.length() - 1) {
                 visited[i] = true;
-                dfs(words[i], target, words, cnt+1);
+                dfs(words[i], target, words, cnt+1, visited);
                 visited[i] = false;
             }
         }
