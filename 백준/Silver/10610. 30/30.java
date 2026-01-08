bf2d85cb33b2d7f36c6fb0498f1918a3499db409
jusuri27 <jusuri27@gmail.com>
2026-01-08 11:07:27 +0900

    [Silver IV] Title: 30, Time: 288 ms, Memory: 21508 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String value = sc.next();
+
+        char[] arr = String.valueOf(value).toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.sort(arr);
+        int sum = 0;
+        for(int i=arr.length-1; i>=0; i--) {
+            int num = arr[i] - '0';
+            sum += num;
+            sb.append(num);
+        }
+        if(arr[0] != '0' || sum % 3 !=0) {
+            System.out.println(-1);
+            return;
+        }
+        System.out.println(sb);
+    }
+}
