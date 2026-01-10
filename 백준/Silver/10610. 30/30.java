708f0b5225ff57908f6ab59493fc4ae1ca1803f5
jusuri27 <jusuri27@gmail.com>
2026-01-10 17:55:41 +0900

    [Silver IV] Title: 30, Time: 152 ms, Memory: 16560 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.java
+++ 백준/Silver/10610. 30/30.java
@@ -1,25 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String value = sc.next();
-
-        char[] arr = String.valueOf(value).toCharArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
+        String input = br.readLine();
+        char[] arr = input.toCharArray();
         Arrays.sort(arr);
-        int sum = 0;
-        for(int i=arr.length-1; i>=0; i--) {
-            int num = arr[i] - '0';
-            sum += num;
-            sb.append(num);
-        }
-        if(arr[0] != '0' || sum % 3 !=0) {
+
+        if(arr[0] != '0') {
             System.out.println(-1);
             return;
         }
+
+        int value = 0;
+        for(int i=arr.length-1; i>=0; i--) {
+            value += arr[i];
+            sb.append(arr[i]);
+        }
+
+        if(value % 3 != 0) {
+            System.out.println(-1);
+            return;
+        }
+
         System.out.println(sb);
     }
 }
