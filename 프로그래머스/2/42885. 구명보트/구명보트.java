62eb2819a7d4be708b3e3f5163ec7016184d33d7
jusuri27 <jusuri27@gmail.com>
2026-01-10 18:27:18 +0900

    [level 2] Title: 구명보트, Time: 11.66 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,21 +1,28 @@
-import java.util.Arrays;
+// 보트 한개에 최대 2명
+
+import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int left = 0;
-        int right = people.length - 1;
-        
         Arrays.sort(people);
+        int left = 0;
+        int right = people.length-1;
         
-        while(left <= right) {
-            if(people[left] + people[right] <= limit) {
-                left++;
+        while(true) {
+            if(left > right) {
+                return answer;
             }
             
+            if(people[left] + people[right] <= limit) {
+                left++;
+                right--;
+            } else {
+                right--;
+            }
             answer++;
-            right--;
         }
-        return answer;
+        
+        // return answer;
     }
 }
