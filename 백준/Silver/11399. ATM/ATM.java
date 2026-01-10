9597f07718e7a35e4b185dde97885443d99ba3e2
jusuri27 <jusuri27@gmail.com>
2026-01-10 15:59:09 +0900

    [Silver IV] Title: ATM, Time: 212 ms, Memory: 20572 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int answer = 0;
-
         Scanner sc = new Scanner(System.in);
-        int people = sc.nextInt();
-        int[] time = new int[people];
-        for(int i=0; i<people; i++) {
-            time[i] = sc.nextInt();
-        }
-        Arrays.sort(time);
+        int count = sc.nextInt();
+        int[] waitTime = new int[count];
 
-        int waitTime = 0;
-        for(int i=0; i<time.length; i++) {
-            waitTime += time[i];
-            answer += waitTime;
+        for(int i = 0; i < count; i++) {
+            waitTime[i] = sc.nextInt();
+        }
+
+        Arrays.sort(waitTime);
+
+        int sum = 0;
+        int answer = 0;
+        for(int i=0; i<waitTime.length; i++) {
+            sum += waitTime[i];
+            answer += sum;
         }
 
         System.out.println(answer);
