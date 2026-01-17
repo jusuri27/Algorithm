9e2215d488703063c61d8145c0a45611b656e3f8
jusuri27 <jusuri27@gmail.com>
2026-01-17 11:46:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 336 ms, Memory: 88964 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int answer = sc.nextInt();
+        int n = sc.nextInt();
 
-        int count = 0;
         int i = 0;
+        int cnt = 0;
         while(true) {
             i++;
             String str = String.valueOf(i);
             if(str.contains("666")) {
-                count++;
+                cnt++;
             }
-            if(answer == count) {
+            if(cnt == n) {
                 System.out.println(i);
-                break;
+                return;
             }
         }
     }
