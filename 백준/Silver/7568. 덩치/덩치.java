3686a19169686bbcc5843051dbe625d3ec9a87e3
jusuri27 <jusuri27@gmail.com>
2026-01-17 11:47:06 +0900

    [Silver V] Title: 덩치, Time: 168 ms, Memory: 17748 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,34 +1,27 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[][] people = new int[num][2];
+        int n = sc.nextInt();
+        int[][] people = new int[n][2];
 
-        for(int i=0; i<num; i++) {
+        for(int i=0; i<people.length; i++) {
             people[i][0] = sc.nextInt();
             people[i][1] = sc.nextInt();
         }
 
-        int[] ranking = new int[num];
-
         for(int i=0; i<people.length; i++) {
-            int value = 1;
+            int ranking = 1;
             for(int j=0; j<people.length; j++) {
                 if(i == j) {
                     continue;
                 }
                 if(people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
-                    value++;
+                    ranking++;
                 }
             }
-            ranking[i] = value;
-        }
-
-        for(int i : ranking) {
-            System.out.print(i+ " ");
+            System.out.println(ranking);
         }
     }
 }
