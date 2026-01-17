d4d7afc21053a0d57f87529eae5d0aa310e434bd
jusuri27 <jusuri27@gmail.com>
2026-01-17 11:27:14 +0900

    [Silver IV] Title: 한수, Time: 180 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -7,18 +7,15 @@ public class Main {
 
         int answer = 0;
         for(int i=1; i<=num; i++) {
-            if(i<100) {
+            if(i < 100) {
                 answer++;
-                continue;
-            }
-            if(i == 1000) {
-                break;
-            }
-            String str = String.valueOf(i);
-            char[] ch = str.toCharArray();
-
-            if(ch[1] - ch[0] == ch[2] - ch[1]) {
-                answer++;
+            } else {
+                int a = i / 100;
+                int b = (i / 10) % 10;
+                int c = i % 10;
+                if(b - a  == c - b) {
+                    answer++;
+                }
             }
         }
         System.out.println(answer);
