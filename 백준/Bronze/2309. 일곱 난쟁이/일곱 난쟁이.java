7f8767afae285faaa32869b19559ef8b2ecf92d2
jusuri27 <jusuri27@gmail.com>
2026-01-17 10:28:01 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 168 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,32 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
+        int[] people = new int[9];
         int sum = 0;
-
-        for(int i=0; i<9; i++) {
-            arr.add(sc.nextInt());
-            sum += arr.get(i);
+        for(int i=0; i<people.length; i++) {
+            people[i] = sc.nextInt();
+            sum += people[i];
         }
-        
-        Loop:
-        for(int i=0; i<arr.size(); i++) {
-            for(int j=i+1; j<arr.size(); j++) {
-                if(sum - (arr.get(i) + arr.get(j)) == 100) {
-                    arr.remove(j);
-                    arr.remove(i);
-                    break Loop;
+        Arrays.sort(people);
+
+        for(int i=0; i<people.length; i++) {
+            for(int j=i+1; j<people.length; j++) {
+                if(sum - people[i] - people[j] == 100) {
+                    people[i] = -1;
+                    people[j] = -1;
+                    for(int k=0; k<people.length; k++) {
+                        if(people[k] >= 0) {
+                            System.out.println(people[k]);
+                        }
+                    }
+                    return;
                 }
             }
         }
 
-        Collections.sort(arr);
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.println(arr.get(i));
-        }
     }
 }
