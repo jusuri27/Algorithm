3293010198dfea3a5fe6baeab863e3268543b182
jusuri27 <jusuri27@gmail.com>
2026-01-18 16:35:16 +0900

    [Bronze II] Title: 분해합, Time: 192 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
+        int n = sc.nextInt();
 
-        for(int i=0; i<num; i++) {
+        for(int i=1; i<=n; i++) {
             int temp = i;
             int sum = 0;
-
-            while(temp>0) {
+            while(temp > 0) {
                 sum += temp % 10;
                 temp /= 10;
             }
-
-            if(sum+i == num) {
+            if(sum+i == n) {
                 System.out.println(i);
                 return;
             }
