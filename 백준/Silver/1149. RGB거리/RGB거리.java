09cdf1417151cba3d18de1c8d4f18f59d263c39d
jusuri27 <jusuri27@gmail.com>
2026-01-21 12:07:34 +0900

    [Silver I] Title: RGB거리, Time: 196 ms, Memory: 19868 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][3];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+            arr[i][2] = sc.nextInt();
+        }
+
+        for(int i=1; i<arr.length; i++) {
+            arr[i][0] += Math.min(arr[i-1][1], arr[i-1][2]);
+            arr[i][1] += Math.min(arr[i-1][0], arr[i-1][2]);
+            arr[i][2] += Math.min(arr[i-1][0], arr[i-1][1]);
+        }
+
+        System.out.println(Math.min(Math.min(arr[n-1][0], arr[n-1][1]), arr[n-1][2]));
+    }
+}
