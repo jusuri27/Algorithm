f9dad97969ecdad2c97b687602cfcd64dbf0abd1
jusuri27 <jusuri27@gmail.com>
2026-01-24 17:16:30 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,14 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n+1];
         arr[0] = 0;
         arr[1] = 1;
 
-        for(int i=2; i<=n; i++) {
+        for(int i=2; i<arr.length; i++) {
             arr[i] = arr[i-2] + arr[i-1];
         }
         System.out.println(arr[n]);
