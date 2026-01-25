d2c3dcd2d8053ae2c77b5976aa72a717fc14f193
jusuri27 <jusuri27@gmail.com>
2026-01-25 19:19:41 +0900

    [Silver III] Title: 파도반 수열, Time: 180 ms, Memory: 17908 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -4,26 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        for(int i=0; i<n; i++) {
-            int p = sc.nextInt();
-            if(p < 3) {
-                System.out.println(1);
-                continue;
-            }
-            System.out.println(find(p));
-        }
-    }
-
-    public static long find(int p) {
-        long[] arr = new long[p];
-        arr[0] = 1;
+        long[] arr = new long[101];
         arr[1] = 1;
         arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 2;
+        arr[5] = 2;
 
-        for(int i=3; i<arr.length; i++) {
-            arr[i] = arr[i-3] + arr[i-2];
+        for(int i=6; i<arr.length; i++) {
+            arr[i] = arr[i-1] + arr[i-5];
         }
-        return arr[arr.length - 1];
+
+        for(int i=0; i<n; i++) {
+            int target = sc.nextInt();
+            System.out.println(arr[target]);
+        }
     }
 }
