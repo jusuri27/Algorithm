e7fc47dbfb0d6e764bc612e7d7e2614e03f578a9
jusuri27 <jusuri27@gmail.com>
2026-01-25 18:01:51 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,39 +1,27 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int value = 0;
-        for(int i=0; i<n; i++) {
-            int floor = sc.nextInt();
-            int room = sc.nextInt();
-            value = count(floor, room);
-            System.out.println(value);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] dp = new int[15][15];
+        for(int i=1; i<dp.length; i++) {
+            dp[0][i] = i;
+            dp[i][1] = 1;
         }
 
-    }
-    public static int count(int floor, int room) {
-        int[][] dp = new int[floor+1][room];
-        for(int i=0; i<dp[0].length; i++) {
-            dp[0][i] = i+1;
-        }
         for(int i=1; i<dp.length; i++) {
-            dp[i][0] = 1;
-            int sum = 0;
-            for(int j=1; j<dp[i].length; j++) {
+            for(int j=2; j<dp.length; j++) {
                 dp[i][j] = dp[i][j-1] + dp[i-1][j];
             }
         }
-        return dp[floor][room-1];
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++) {
+            int floor = Integer.parseInt(br.readLine());
+            int room = Integer.parseInt(br.readLine());
+            System.out.println(dp[floor][room]);
+        }
     }
 }
-
-/*
-
-1   4
-1   3   6   10  16  22  29  37  46
-1   2   3   4   5   6   7   8   9
- */
