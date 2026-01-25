4d323f16ae22f49f718b31dc7e7dc3c2a12271ac
jusuri27 <jusuri27@gmail.com>
2026-01-25 19:12:38 +0900

    [Silver I] Title: 정수 삼각형, Time: 784 ms, Memory: 129732 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -2,39 +2,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int n;
-    static int[][] arr;
     static Integer[][] dp;
+    static int[][] arr;
+    static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         arr = new int[n][n];
         dp = new Integer[n][n];
-
         for(int i=0; i<n; i++) {
-            for(int j=0; j<i+1; j++) {
+            for(int j=0; j<=i; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<arr.length; i++) {
             dp[n-1][i] = arr[n-1][i];
         }
         System.out.println(find(0, 0));
     }
-
-    public static int find(int depth, int idx) {
+    public static int find(int idx, int depth) {
         if(depth == n-1) {
             return dp[depth][idx];
         }
-
         if(dp[depth][idx] == null) {
-            dp[depth][idx] = Math.max(find(depth+1, idx), find(depth+1, idx+1)) + arr[depth][idx];
+            dp[depth][idx] = Math.max(find(idx, depth+1), find(idx+1,depth+1)) + arr[depth][idx];
         }
         return dp[depth][idx];
     }
 }
-
 /*
     7
    3 8
