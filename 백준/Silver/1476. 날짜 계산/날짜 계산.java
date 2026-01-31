bab0c61a91e1b842fb18e14aeb8bf513a97b878c
jusuri27 <jusuri27@gmail.com>
2026-01-31 21:30:56 +0900

    [Silver V] Title: 날짜 계산, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -1,36 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int e = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
         int answer = 0;
-
-        int earth = sc.nextInt();
-        int sun = sc.nextInt();
-        int moon = sc.nextInt();
-
-        int[] arr = {1, 1, 1};
-
+        int e1 = 1;
+        int s1 = 1;
+        int m1 = 1;
         while(true) {
             answer++;
-            if(earth == arr[0] && sun == arr[1] && moon == arr[2]) {
-                System.out.println(answer);
+            if(e == e1 && s == s1 && m == m1) {
                 break;
             }
-            arr[0]++;
-            if(arr[0] == 16) {
-                arr[0] = 1;
+            e1++;
+            s1++;
+            m1++;
+            if(e1 == 16) {
+                e1 = 1;
             }
-
-            arr[1]++;
-            if(arr[1] == 29) {
-                arr[1] = 1;
+            if(s1 == 29) {
+                s1 = 1;
             }
-
-            arr[2]++;
-            if(arr[2] == 20) {
-                arr[2] = 1;
+            if(m1 == 20) {
+                m1 = 1;
             }
         }
+        System.out.println(answer);
     }
 }
