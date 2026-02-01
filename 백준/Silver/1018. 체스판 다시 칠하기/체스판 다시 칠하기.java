a3bde4b1eb858971affa6295bb4f4212f1b19d15
jusuri27 <jusuri27@gmail.com>
2026-02-01 18:54:17 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,52 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static boolean[][] arr;
-    static int min = 64;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    static int answer = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         arr = new boolean[n][m];
-
         for(int i=0; i<arr.length; i++) {
-            String str = sc.next();
+            String line = br.readLine();
             for(int j=0; j<arr[0].length; j++) {
-                char ch = str.charAt(j);
-                if(ch == 'W') {
+                if(line.charAt(j) == 'W') {
                     arr[i][j] = true;
                 }
             }
         }
 
-        int xRange = n - 8;
-        int yRange = m - 8;
-        for(int i=0; i<=xRange; i++) {
-            for(int j=0; j<=yRange; j++) {
-                find(i, j);
-            }
-        }
-        System.out.println(min);
+        int xRange = n - 7;
+        int yRange = m - 7;
+
+        find(xRange, yRange);
+        System.out.println(answer);
     }
 
     public static void find(int x, int y) {
-        int xMax = x+8;
-        int yMax = y+8;
-        int count = 0;
-        boolean check = arr[x][y];
+        for(int i=0; i<x; i++) {
+            for(int j=0; j<y; j++) {
+                cal(i, j);
+            }
+        }
+    }
 
+    public static void cal(int x, int y) {
+        int xMax = x + 8;
+        int yMax = y + 8;
+        boolean check = arr[x][y];
+        int count = 0;
         for(int i=x; i<xMax; i++) {
             for(int j=y; j<yMax; j++) {
-                if(check != arr[i][j]) {
+                if(arr[i][j] != check) {
                     count++;
                 }
                 check = !check;
             }
             check = !check;
         }
-
-        count = Math.min(count, 64-count);
-        min = Math.min(count, min);
+        count = Math.min(count, 64 - count);
+        answer = Math.min(answer, count);
     }
 }
