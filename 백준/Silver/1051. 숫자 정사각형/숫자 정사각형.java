7992bc336f29a55217ce57947089e0634b0cc131
jusuri27 <jusuri27@gmail.com>
2026-02-01 17:08:06 +0900

    [Silver III] Title: 숫자 정사각형, Time: 96 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.java
@@ -2,33 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     static int x;
     static int y;
     static int[][] arr;
-    static int answer = 1;
+    static int answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         x = Integer.parseInt(st.nextToken());
         y = Integer.parseInt(st.nextToken());
-        arr = new int[x][y];
 
+
+        arr = new int[x][y];
         for(int i=0; i<arr.length; i++) {
             String line = br.readLine();
             for(int j=0; j<arr[0].length; j++) {
-                arr[i][j] = Character.getNumericValue(line.charAt(j));
+                arr[i][j] = line.charAt(j) - '0';
             }
         }
 
-        if(x == 1 || y == 1) {
-            System.out.println(answer);
-            return;
-        }
-        find(2, 2, 1);
+        find(1, 1, 0);
         System.out.println(answer);
     }
 
@@ -36,14 +32,14 @@ public class Main {
         if(nx > x || ny > y) {
             return;
         }
-        for(int i=0; i<=arr.length - nx; i++) {
-            for(int j=0; j<=arr[0].length - ny; j++) {
+        for(int i=0; i<=x-nx; i++) {
+            for(int j=0; j<=y-ny; j++) {
                 int a = arr[i][j];
-                int b = arr[i][j + count];
-                int c = arr[i + count][j];
-                int d = arr[i + count][j + count];
+                int b = arr[i][j+count];
+                int c = arr[i+count][j];
+                int d = arr[i+count][j+count];
                 if(a == b && b == c && c == d) {
-                    answer = Math.max(answer, nx * ny);
+                    answer = nx * ny;
                 }
             }
         }
