009eeff158b4e047992d50c85f55df968bcfc90c
jusuri27 <jusuri27@gmail.com>
2026-02-02 10:59:54 +0900

    [Silver IV] Title: 보물, Time: 192 ms, Memory: 18004 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        Integer[] b = new Integer[n];
+
+        for(int i=0; i<a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<b.length; i++) {
+            b[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        Arrays.sort(b, Comparator.reverseOrder());
+
+        int min = 0;
+        for(int i=0; i<a.length; i++) {
+            min += a[i] * b[i];
+        }
+
+        System.out.println(min);
+    }
+}
