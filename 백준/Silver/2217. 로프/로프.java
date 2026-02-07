a72ab9b83d9da0682794f9f4aca6f14fabda2461
jusuri27 <jusuri27@gmail.com>
2026-02-07 10:39:59 +0900

    [Silver IV] Title: 로프, Time: 1016 ms, Memory: 105404 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -2,36 +2,24 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
 
-// 무게를 내림차순으로 정렬
-// 가장 무거운 로프부터 n, n+1, n+2 의 합계를 구함
 public class Main {
-    static int n;
-    static Integer[] arr;
-    static int max;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        arr = new Integer[n];
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n+1];
+        arr[0] = Integer.MAX_VALUE;
 
-        for(int i=0; i<arr.length; i++) {
+        for(int i=1; i<arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr, Comparator.reverseOrder());
 
-        max = arr[0];
-        find(1);
+        int max = 0;
+        for(int i=1; i<arr.length; i++) {
+            int sum = arr[i] * i;
+            max = Math.max(max, sum);
+        }
+        
         System.out.println(max);
-    }
-
-    public static void find(int count) {
-        if(count == n) {
-            return;
-        }
-        int average = 0;
-        for(int i=0; i<count; i++) {
-            average = Math.min(arr[i], arr[i+1]);
-        }
-        max = Math.max(max, average * (count + 1));
-        find(count + 1);
     }
 }
