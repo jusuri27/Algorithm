102c049b4bf695b20d7969ff617494a2ff403524
jusuri27 <jusuri27@gmail.com>
2026-02-08 16:18:31 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -1,28 +1,28 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
 
         for(int i=0; i<arr.length; i++) {
-            arr[i] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int count = 0;
-        for(int i=arr.length - 1; i>0; i--) {
-            if(arr[i] == arr[i-1]) {
-                arr[i-1]--;
-                count++;
-            }
-            if(arr[i] < arr[i-1]) {
-                int value = arr[i-1] - arr[i] + 1;
-                arr[i-1] -= value;
-                count += value;
+        int answer = 0;
+        for(int i=arr.length-1; i>0; i--) {
+            if(arr[i-1] >= arr[i]) {
+                int count = arr[i-1] - arr[i] + 1;
+                arr[i-1] = arr[i-1] - count;
+                answer += count;
             }
         }
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
