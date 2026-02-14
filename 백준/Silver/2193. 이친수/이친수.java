0e8da578d379dcb7891286958ae4e358603df380
jusuri27 <jusuri27@gmail.com>
2026-02-14 10:38:17 +0900

    [Silver III] Title: 이친수, Time: 168 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -4,40 +4,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] arr = new long[n+1];
-        if(n <= 2) {
-            System.out.println(1);
-            return;
+        long[] dp = new long[n+1];
+        dp[1] = 1;
+        if(n > 1) {
+            dp[2] = 1;
         }
-        arr[1] = 1;
-        arr[2] = 1;
-        for(int i=3; i<arr.length; i++) {
-            arr[i] = arr[i-2] + arr[i-1];
+
+        for(int i=3; i<dp.length; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
         }
-        System.out.println(arr[n]);
+        System.out.println(dp[n]);
     }
 }
 
 /*
-n = 1
 1
 
-n = 2
 10
 
-n = 3
-101
 100
+101
 
-n = 4
 1000
 1001
 1010
 
-n = 5
 10000
 10001
 10010
 10100
 10101
+
+100000
+100001
+100010
+100100
+100101
+101000
+101001
+101010
+
+
+
+
  */
