e7cfc6ab09f8a20317e82a68485e268c42958e5f
jusuri27 <jusuri27@gmail.com>
2026-02-14 10:58:44 +0900

    [Silver V] Title: BABBA, Time: 180 ms, Memory: 18376 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.java
+++ 백준/Silver/9625. BABBA/BABBA.java
@@ -5,29 +5,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] dp = new int[n+1][2];
-        dp[0][0] = 1;
-        if(n >= 1) {
-            dp[1][1] = 1;
+        dp[1][1] = 1;
+        if(n > 1) {
+            dp[2][0] = 1;
+            dp[2][1] = 1;
         }
 
         for(int i=2; i<dp.length; i++) {
             dp[i][0] = dp[i-1][1];
             dp[i][1] = dp[i-1][0] + dp[i-1][1];
         }
-
         System.out.println(dp[n][0] + " " + dp[n][1]);
     }
 }
 
 /*
+0
+A
 
-0 A                                     1 0
-1 B                                     0 1
-2 BA                                    1 1
-3 BAB                                   1 2
-4 BABBA                                 2 3
-5 BABBABAB                              3 5
-6 BABBABABBABBA                         5 8
-7 BABBABABBABBABABBABAB                 8 13
-8 BABBABABBABBABABBABABBABBABABBABBA    13 21
+1
+B
+
+2
+BA
+
+3
+BAB
+
+4
+BABBA
+
+5
+BABBABAB
  */
