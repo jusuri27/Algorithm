d2260e34c1c434b54bebdaec71db0abd8e7efd89
jusuri27 <jusuri27@gmail.com>
2026-02-14 19:41:51 +0900

    [Silver I] Title: 포도주 시식, Time: 268 ms, Memory: 29444 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -4,23 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        int[] dp = new int[n];
-
-        for(int i=0; i<arr.length; i++) {
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        for(int i=1; i<arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        dp[0] = arr[0];
+
+        dp[1] = arr[1];
         if(n > 1) {
-            dp[1] = arr[0] + arr[1];
+            dp[2] = arr[1] + arr[2];
         }
-        if(n > 2) {
-            dp[2] = Math.max(dp[1], Math.max(arr[0] + arr[2], arr[1] + arr[2]));
-        }
-        for(int i=3; i<arr.length; i++) {
+        for(int i=3; i<dp.length; i++) {
             dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
         }
-
-        System.out.println(dp[n-1]);
+        System.out.println(dp[n]);
     }
 }
+
+/*
+1 2 3
+1 2
+1 3
+0 2 3
+
+1 2 3 4
+1 3 4
+1 2 4
+2 3
+
+1 2 3 4 5
+1 2 4 5
+1 3 4
+1 3 5
+2 3 5
+
+dp[i-3] + arr[i-1] + arr[i]
+dp[3]
+dp[i-2] + arr[i]
+
+ */
