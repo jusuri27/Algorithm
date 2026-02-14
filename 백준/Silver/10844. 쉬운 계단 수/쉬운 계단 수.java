d22fd7b24e49d6240abfa53b75e5ad69c9fc861c
jusuri27 <jusuri27@gmail.com>
2026-02-14 11:55:37 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 148 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,51 +1,49 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] dp = new int[n+1][10];
         int mod = 1000000000;
-        long[][] arr = new long[n+1][10];
-        for(int i=1; i<arr[0].length; i++) {
-            arr[1][i] = 1;
+        for(int i=1; i<10; i++) {
+            dp[1][i] = 1;
         }
-
-        for(int i=2; i<arr.length; i++) {
-            for(int j=0; j<arr[i].length; j++) {
+        for(int i=2; i<dp.length; i++) {
+            for(int j=0; j<dp[i].length; j++) {
                 if(j == 0) {
-                    arr[i][j] = arr[i-1][1] % mod;
-                } else if (j == 9) {
-                    arr[i][j] = arr[i-1][8] % mod;
+                    dp[i][j] = dp[i-1][j+1] % mod;
+                } else if(j == 9) {
+                    dp[i][j] = dp[i-1][j-1] % mod;
                 } else {
-                    arr[i][j] = (arr[i-1][j-1] + arr[i-1][j+1]) % mod;
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % mod;
                 }
             }
         }
-
         long sum = 0;
-        for(int i=0; i<arr[0].length; i++) {
-            sum += arr[n][i];
+        for(int i=0; i<10; i++) {
+            sum += dp[n][i];
         }
         System.out.println(sum % mod);
     }
 }
 
 /*
-10
-12
+10 12
+21 23
+32 34
+43 45
+76 78
+87 89
 98
 
-101
-121
-123
+101 121 123
+210 212 232 234
+321 323 343 345
+876 878 898
+987 989
 
-210
-213
-232
-234
-
-1010
-1012
-12
+1010 1012 1210 1212 1232 1234
 
  */
