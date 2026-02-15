c6f4a3681d3d7a5af592bfb605e3cb4d0e10444b
jusuri27 <jusuri27@gmail.com>
2026-02-15 18:15:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 15396 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,22 +1,25 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         int[] dp = new int[n];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0; i<arr.length; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         for(int i=0; i<dp.length; i++) {
             dp[i] = 1;
-
             for(int j=0; j<i; j++) {
-                if(arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                if(arr[i] > arr[j] && dp[i] < dp[j] + 1) {
                     dp[i] = dp[j] + 1;
                 }
             }
