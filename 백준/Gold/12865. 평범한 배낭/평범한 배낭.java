d9de70d22ce3d3916f5e7693994bcd6fcf6c1433
jusuri27 <jusuri27@gmail.com>
2026-02-15 21:05:46 +0900

    [Gold V] Title: 평범한 배낭, Time: 220 ms, Memory: 56912 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,25 +6,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-
-        int[] w = new int[n+1];
-        int[] v = new int[n+1];
+        int[][] arr = new int[n+1][2];
         int[][] dp = new int[n+1][k+1];
 
-        for(int i=1; i<=n; i++) {
-            w[i] = sc.nextInt();
-            v[i] = sc.nextInt();
+        for(int i=1; i<arr.length; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
         }
 
-        for(int i=1; i<dp.length; i++) {
-            for(int j=1; j<dp[i].length; j++) {
-                if(j < w[i]) {
+        for(int i=1; i<arr.length; i++) {
+            for(int j=1; j<dp[0].length; j++) {
+                if(arr[i][0] > j) {
                     dp[i][j] = dp[i-1][j];
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j - w[i]] + v[i]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]] + arr[i][1]);
                 }
             }
-//            System.out.println(Arrays.toString(dp[i]));
         }
         System.out.println(dp[n][k]);
     }
