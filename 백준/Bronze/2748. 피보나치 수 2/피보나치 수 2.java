320edd17f891405a7b5d05f2b9ff9ebe0227f3b6
jusuri27 <jusuri27@gmail.com>
2026-02-15 17:00:34 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 172 ms, Memory: 17768 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -7,11 +7,9 @@ public class Main {
         long[] arr = new long[n+1];
         arr[0] = 0;
         arr[1] = 1;
-
         for(int i=2; i<arr.length; i++) {
-            arr[i] = arr[i-1] + arr[i-2];
+            arr[i] = arr[i-2] + arr[i-1];
         }
-
         System.out.println(arr[n]);
     }
 }
