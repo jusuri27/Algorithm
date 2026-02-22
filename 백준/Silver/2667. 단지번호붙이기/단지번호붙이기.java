8180e29b5d6445e23e97fba56af4e81caf613892
jusuri27 <jusuri27@gmail.com>
2026-02-22 19:46:14 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,45 +4,45 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
     static int[][] arr;
     static boolean[][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         arr = new int[n][n];
         visited = new boolean[n][n];
+
         for(int i=0; i<arr.length; i++) {
             String str = br.readLine();
-            for(int j=0; j<str.length(); j++) {
-                int value = str.charAt(j) - '0';
-                arr[i][j] = value;
+            for(int j=0; j<arr[i].length; j++) {
+                arr[i][j] = str.charAt(j) - '0';
             }
         }
 
-        int cnt = 0;
-        ArrayList<Integer> answer = new ArrayList<>();
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
         for(int i=0; i<arr.length; i++) {
             for(int j=0; j<arr[i].length; j++) {
-                if(arr[i][j] == 1 && !visited[i][j]) {
-                    cnt++;
-                    int value = bfs(i, j);
-                    answer.add(value);
+                if(!visited[i][j] && arr[i][j] == 1) {
+                    count++;
+                    list.add(bfs(i, j));
                 }
             }
         }
-        System.out.println(cnt);
-        Collections.sort(answer);
-        for(int i=0; i<answer.size(); i++) {
-            System.out.println(answer.get(i));
+        System.out.println(count);
+        Collections.sort(list);
+        for(int i : list) {
+            System.out.println(i);
         }
     }
 
     public static int bfs(int x, int y) {
+        visited[x][y] = true;
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{x, y});
-        visited[x][y] = true;
+
         int cnt = 1;
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -54,14 +54,15 @@ public class Main {
                 int ny = cy + dy[i];
 
                 if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length) {
-                    if(arr[nx][ny] == 1 && !visited[nx][ny]) {
+                    if(!visited[nx][ny] && arr[nx][ny] == 1) {
                         visited[nx][ny] = true;
                         cnt++;
-                        queue.add(new int[]{nx, ny, cnt});
+                        queue.add(new int[]{nx, ny});
                     }
                 }
             }
         }
+
         return cnt;
     }
 }
