b75c3277e0bf721db80c7ad1c4be0c4be3c4b8ac
jusuri27 <jusuri27@gmail.com>
2026-02-22 19:30:40 +0900

    [Silver II] Title: 섬의 개수, Time: 148 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,19 +1,21 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    // 동 동남, 남, 서남, 서, 서북, 북, 동북
+    // 동 남동, 남, 남서, 서, 북서, 북, 북동
     static int[] dx = {1, 1, 0, -1, -1, -1, 0, 1};
     static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-    static int[][] arr;
     static boolean[][] visited;
+    static int[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         while(true) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
             if(n == 0 && m == 0) {
                 break;
             }
@@ -22,39 +24,42 @@ public class Main {
             visited = new boolean[m][n];
 
             for(int i=0; i<arr.length; i++) {
+                st = new StringTokenizer(br.readLine());
                 for(int j=0; j<arr[i].length; j++) {
-                    arr[i][j] = sc.nextInt();
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            int answer = 0;
+            int count = 0;
             for(int i=0; i<arr.length; i++) {
                 for(int j=0; j<arr[i].length; j++) {
-                    if(arr[i][j] == 1 && !visited[i][j]) {
-                        answer++;
+                    if(!visited[i][j] && arr[i][j] == 1) {
+                        count++;
                         bfs(i, j);
                     }
                 }
             }
-            System.out.println(answer);
+            System.out.println(count);
         }
     }
 
     public static void bfs(int x, int y) {
+        visited[x][y] = true;
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{x, y});
-        visited[x][y] = true;
+
 
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
             int cx = current[0];
             int cy = current[1];
 
-            for(int i=0; i<dx.length; i++) {
+            for(int i=0; i<dx.length ;i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length) {
-                    if(arr[nx][ny] == 1 && !visited[nx][ny]) {
+
+                if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length ) {
+                    if(!visited[nx][ny] && arr[nx][ny] == 1) {
                         visited[nx][ny] = true;
                         queue.add(new int[]{nx, ny});
                     }
