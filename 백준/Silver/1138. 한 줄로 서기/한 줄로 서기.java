89c7a4c7d0cd4fd1badf602a76ed11ad8ae3d6d2
jusuri27 <jusuri27@gmail.com>
2026-02-26 10:54:40 +0900

    [Silver II] Title: 한 줄로 서기, Time: 192 ms, Memory: 18312 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=arr.length-1; i>=0; i--) {
+            list.add(arr[i], i+1);
+        }
+        for(int i : list) {
+            System.out.print(i + " ");
+        }
+    }
+}
