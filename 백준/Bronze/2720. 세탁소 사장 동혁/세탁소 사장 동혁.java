62784a70c94f471068cf0ce38c34447cac792054
jusuri27 <jusuri27@gmail.com>
2026-02-28 10:49:44 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 168 ms, Memory: 15768 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,37 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         for(int i=0; i<n; i++) {
-            int value = sc.nextInt();
-            solve(value);
-            System.out.println();
+            solve(Integer.parseInt(br.readLine()));
         }
     }
-
     public static void solve(int value) {
-        int q = 25;
-        int d = 10;
-        int n = 5;
-        int p = 1;
-        int[] arr = new int[4];
+        int[] arr = {25, 10, 5, 1};
 
-        arr[0] = value / q;
-        value = value % q;
-
-        arr[1] = value / d;
-        value = value % d;
-
-        arr[2] = value / n;
-        value = value % n;
-
-        arr[3] = value / p;
-
-        for(int i : arr) {
-            System.out.print(i + " ");
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(value / arr[i] + " ");
+            value = value % arr[i];
         }
+        System.out.println();
     }
 }
