4e4d975d3574896724e3899e61bdb0ac9ea73db3
jusuri27 <jusuri27@gmail.com>
2026-03-01 22:18:49 +0900

    [Silver II] Title: 주식, Time: 1000 ms, Memory: 316196 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         for(int i=0; i<t; i++) {
-            int n = sc.nextInt();
+            int n = Integer.parseInt(br.readLine());
             int[] arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0; j<arr.length; j++) {
-                arr[j] = sc.nextInt();
+                arr[j] = Integer.parseInt(st.nextToken());
             }
-
-            int max = arr[arr.length - 1];
+            int max = arr[arr.length-1];
             long answer = 0;
             for(int k=arr.length-2; k>=0; k--) {
                 if(arr[k] > max) {
                     max = arr[k];
-                } else {
-                    answer += max - arr[k];
                 }
+                answer += max - arr[k];
             }
             System.out.println(answer);
         }
