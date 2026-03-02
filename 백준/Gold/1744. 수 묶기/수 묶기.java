8bddff3057d440d6d3bb44547a6de242dc7c0042
jusuri27 <jusuri27@gmail.com>
2026-03-02 18:36:48 +0900

    [Gold IV] Title: 수 묶기, Time: 108 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,44 +1,47 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int sum;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        ArrayList<Integer> positive = new ArrayList<>();
-        ArrayList<Integer> negative = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        List<Integer> minus = new ArrayList<>();
+        List<Integer> plus = new ArrayList<>();
 
         for(int i=0; i<n; i++) {
-            int value = sc.nextInt();
+            int value = Integer.parseInt(br.readLine());
             if(value == 1) {
-                sum++;
+                sum += 1;
+                continue;
+            }
+            if(value > 0) {
+                plus.add(value);
             }
             if(value <= 0) {
-                negative.add(value);
-            }
-            if(value > 1) {
-                positive.add(value);
+                minus.add(value);
             }
         }
+        Collections.sort(minus);
+        Collections.sort(plus, Comparator.reverseOrder());
 
-        Collections.sort(positive);
-        Collections.sort(negative, Comparator.reverseOrder());
+        int plusSum = solve(plus);
+        int minusSum = solve(minus);
 
-        cal(positive);
-        cal(negative);
-
+        sum += plusSum + minusSum;
         System.out.println(sum);
     }
 
-    public static void cal(ArrayList<Integer> list) {
-        for(int i=list.size()-1; i>0; i-=2) {
-            sum += list.get(i) * list.get(i-1);
+    public static int solve(List<Integer> arr) {
+        int sum = 0;
+        for(int i=0; i<arr.size()-1; i+=2) {
+            sum += arr.get(i) * arr.get(i+1);
         }
-        if(list.size() % 2 == 1) {
-            sum += list.get(0);
+        if(arr.size() % 2 == 1) {
+            sum += arr.get(arr.size()-1);
         }
+        return sum;
     }
 }
