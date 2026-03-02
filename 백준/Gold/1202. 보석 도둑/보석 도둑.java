b3d5dc5025e1b85969dd697f5b5e94600792ddc9
jusuri27 <jusuri27@gmail.com>
2026-03-02 19:23:16 +0900

    [Gold II] Title: 보석 도둑, Time: 1564 ms, Memory: 133376 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,54 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[][] arr = new int[n][2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] bag = new int[n][2];
         int[] weight = new int[k];
 
-        for(int i=0; i<arr.length; i++) {
-            arr[i][0] = sc.nextInt();
-            arr[i][1] = sc.nextInt();
+        for(int i=0; i<bag.length; i++) {
+            st = new StringTokenizer(br.readLine());
+            bag[i][0] = Integer.parseInt(st.nextToken());
+            bag[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for(int i=0; i<weight.length; i++) {
-            weight[i] = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            weight[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (a, b) -> {
+        Arrays.sort(bag, (a, b) -> {
             return a[0] - b[0];
         });
         Arrays.sort(weight);
-
-        int j = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+
         long sum = 0;
+        int idx = 0;
         for(int i=0; i<weight.length; i++) {
-            while(j < arr.length && arr[j][0] <= weight[i]) {
-                pq.add(arr[j][1]);
-                j++;
+            while(bag.length > idx && weight[i] >= bag[idx][0]) {
+                pq.add(bag[idx][1]);
+                idx++;
             }
-            if(!pq.isEmpty()) {
-                int value = pq.poll();
-                sum += value;
+            if(pq.size() > 0) {
+                sum += pq.poll();
             }
         }
         System.out.println(sum);
+
     }
 }
-
-/*
-
-3 2
-10 10
-5 8
-100 100
-11
-9
-
- */
