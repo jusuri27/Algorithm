cf025f25e18ac8b434bec1b5ff2b0785e821df9d
jusuri27 <jusuri27@gmail.com>
2026-03-07 10:35:19 +0900

    [Bronze II] Title: 완전제곱수, Time: 108 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.java
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.java
@@ -1,26 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        int count = 1;
-        int sum = 0;
         int min = Integer.MAX_VALUE;
-        while(count * count <= n) {
+        int answer = 0;
+        int count = 1;
+        while(count * count <= m) {
             int value = count * count;
-            if(value >= m && value <= n) {
-                sum += value;
+            if(value >= n) {
+                answer += value;
                 min = Math.min(min, value);
             }
             count++;
         }
-        if(sum == 0) {
+        if(answer == 0) {
             System.out.println(-1);
         } else {
-            System.out.println(sum);
+            System.out.println(answer);
             System.out.println(min);
         }
     }
