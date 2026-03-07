d95cac6dd786e7690ab937d726cc7111ce6feba1
jusuri27 <jusuri27@gmail.com>
2026-03-07 10:43:38 +0900

    [Silver V] Title: 문서 검색, Time: 188 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -3,21 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String doc = sc.nextLine();
-        String word = sc.nextLine();
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        a = a.replace(b, "*");
 
-        int n = 0;
-        while(n<doc.length()) {
-            n++;
-            if(doc.contains(word)) {
-                doc = doc.replaceFirst(word, "*");
-                n = 0;
-            }
-        }
-        
         int answer = 0;
-        for(int i=0; i<doc.length(); i++) {
-            if(doc.charAt(i) == '*') {
+        for(int i=0; i<a.length(); i++) {
+            if(a.charAt(i) == '*') {
                 answer++;
             }
         }
