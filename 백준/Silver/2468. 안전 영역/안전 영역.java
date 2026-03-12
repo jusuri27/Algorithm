de38d8e4faaf54171198c8ec74781f77d9adad70
jusuri27 <jusuri27@gmail.com>
2026-03-12 12:17:02 +0900

    [Silver I] Title: 안전 영역, Time: 512 ms, Memory: 49592 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,23 +1,19 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int[][] arr;
-    static int[][] temp;
+    static int n;
     static boolean[][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static int max = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         arr = new int[n][n];
-
         int maxNum = 0;
         for(int i=0; i<arr.length; i++) {
-            for(int j=0; j<arr[i].length; j++) {
+            for(int j=0; j<arr.length; j++) {
                 int value = sc.nextInt();
                 arr[i][j] = value;
                 maxNum = Math.max(maxNum, value);
@@ -25,37 +21,28 @@ public class Main {
         }
 
         for(int i=0; i<=maxNum; i++) {
-            init(i);
+            solve(i);
         }
         System.out.println(max);
     }
 
-    public static void init(int n) {
-        temp = new int[arr.length][arr.length];
-        visited = new boolean[arr.length][arr.length];
+    public static void solve(int target) {
+        visited = new boolean[n][n];
+        int count = 0;
         for(int i=0; i<arr.length; i++) {
             for(int j=0; j<arr[i].length; j++) {
-                if(arr[i][j] > n) {
-                    temp[i][j] = -1;
-                }
-            }
-        }
-
-        int count = 0;
-        for(int i=0; i<temp.length; i++) {
-            for(int j=0; j<temp[i].length; j++) {
-                if(temp[i][j] == -1 && !visited[i][j]) {
+                if(!visited[i][j] && arr[i][j] > target) {
                     count++;
-                    visited[i][j] = true;
-                    bfs(i, j);
+                    bfs(i, j, target);
                 }
             }
         }
         max = Math.max(max, count);
     }
 
-    public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
+    public static void bfs(int x, int y, int target) {
+        visited[x][y] = true;
+        Queue<int[]> queue = new ArrayDeque<>();
         queue.add(new int[]{x, y});
 
         while(!queue.isEmpty()) {
@@ -67,8 +54,8 @@ public class Main {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
-                if(nx >=0 && ny >= 0 && nx < temp.length && ny < temp[0].length) {
-                    if(temp[nx][ny] == -1 && !visited[nx][ny]) {
+                if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length) {
+                    if(!visited[nx][ny] && arr[nx][ny] > target) {
                         visited[nx][ny] = true;
                         queue.add(new int[]{nx, ny});
                     }
@@ -77,3 +64,11 @@ public class Main {
         }
     }
 }
+
+
+/*
+최대 영역을 구한다
+높이가 1일때
+높이가 2일때
+높이가 최댓값일때
+ */
