c70abe231da7aa46e94172ee0b18567a87ce6e0e
jusuri27 <jusuri27@gmail.com>
2026-03-13 11:58:57 +0900

    [Gold IV] Title: 가르침, Time: 360 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -4,65 +4,72 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[] learned = new boolean[26];
+    static boolean[] word = new boolean[26];
     static String[] arr;
-    static int r;
-    static int max = 0;
+    static int num;
+    static int max;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        r = k - 5;
+        num = k - 5;
         arr = new String[n];
-        if(r < 0) {
+
+        init();
+
+        if(num < 0) {
             System.out.println(0);
             return;
         }
-        learned['a' - 'a'] = true;
-        learned['n' - 'a'] = true;
-        learned['t' - 'a'] = true;
-        learned['i' - 'a'] = true;
-        learned['c' - 'a'] = true;
 
         for(int i=0; i<arr.length; i++) {
             arr[i] = br.readLine();
         }
-        solve(0, 0);
+
+        dfs(0, 0);
         System.out.println(max);
     }
 
-    public static void solve(int depth, int start) {
-        if(depth == r) {
-            int count = find();
-            max = Math.max(max, count);
+    public static void dfs(int depth, int start) {
+        if(depth == num) {
+            int result = solve();
+            max = Math.max(max, result);
             return;
         }
 
-        for(int i=start; i<learned.length; i++) {
-            if(!learned[i]) {
-                learned[i] = true;
-                solve(depth + 1, i + 1);
-                learned[i] = false;
+        for(int i=start; i<word.length; i++) {
+            if(!word[i]) {
+                word[i] = true;
+                dfs(depth + 1, i + 1);
+                word[i] = false;
             }
         }
     }
 
-    public static int find() {
-        int answer = 0;
+    public static int solve() {
+        int count = 0;
         for(int i=0; i<arr.length; i++) {
+            String str = arr[i];
             boolean check = true;
-            for(int j=0; j<arr[i].length(); j++) {
-                int value = arr[i].charAt(j) - 'a';
-                if(!learned[value]) {
+            for(int j=0; j<str.length(); j++) {
+                if(!word[str.charAt(j)-'a']) {
                     check = false;
                     break;
                 }
             }
             if(check) {
-                answer++;
+                count++;
             }
         }
-        return answer;
+        return count;
+    }
+
+    public static void init() {
+        word['a'-'a'] = true;
+        word['n'-'a'] = true;
+        word['t'-'a'] = true;
+        word['i'-'a'] = true;
+        word['c'-'a'] = true;
     }
 }
