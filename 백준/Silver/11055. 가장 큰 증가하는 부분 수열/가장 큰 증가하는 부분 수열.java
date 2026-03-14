5026006b8598354e9b7a1977638fcf09d39fda58
jusuri27 <jusuri27@gmail.com>
2026-03-14 10:12:25 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 252 ms, Memory: 21220 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.java
@@ -8,16 +8,17 @@ public class Main {
         int[] arr = new int[n];
         int[] dp = new int[n];
 
-        for(int i =0; i<arr.length; i++) {
+        for(int i=0; i<arr.length; i++) {
             arr[i] = sc.nextInt();
         }
 
+        dp[0] = arr[0];
         int max = 0;
-        for(int i=0; i<arr.length; i++) {
+        for(int i=0; i<dp.length; i++) {
             dp[i] = arr[i];
-            for(int j=0; j<i; j++) {
+            for(int j=0; j<=i; j++) {
                 if(arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[j]+arr[i], dp[i]);
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
                 }
             }
             max = Math.max(max, dp[i]);
@@ -25,13 +26,13 @@ public class Main {
         System.out.println(max);
     }
 }
+
 /*
 1 100 2 50 60
-
-dp[0] = 1
-dp[1] = 101
-dp[2] = 3
-dp[3] = 53
-dp[4] = 113
+dp[1] = 1;
+dp[2] = 101;
+dp[3] = 3;
+dp[4] = 53;
+dp[5] = 113;
 
  */
