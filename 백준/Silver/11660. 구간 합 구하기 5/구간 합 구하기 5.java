cbc7b5da4e48901f3123a25bc5eb8a16ed019d74
jusuri27 <jusuri27@gmail.com>
2026-03-14 11:58:45 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2700 ms, Memory: 319988 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,32 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
         int[][] arr = new int[n+1][n+1];
-        for(int i=1; i<arr.length; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=1; j<arr[i].length; j++) {
-                arr[i][j] = arr[i][j-1] + arr[i-1][j] - arr[i-1][j-1] + Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n+1][n+1];
+
+
+        for(int i=1; i<dp.length; i++) {
+            for(int j=1; j<dp[i].length; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] + sc.nextInt() - dp[i-1][j-1];
             }
         }
 
         for(int i=0; i<m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
 
-            int value = arr[x2][y2] - arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1];
+            int value = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1];
             System.out.println(value);
         }
-
     }
 }
+
+/*
+dp 모든 구간 합계 구하는 부분
+dp[i-1][j] + dp[i][j-1] + arr[i][j] + dp[i-1][j-1];
+
+범위값 구하는법
+전체 - 가로 - 세로 + 겹치는 부분
+dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+
+1 2 3 4
+2 3x 4 5
+3 4 5 6
+4 5 6 7
+
+ */
