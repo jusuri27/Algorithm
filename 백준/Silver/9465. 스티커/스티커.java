ecc862863e947d9ccfe89678bd2ecf6d057b5f38
jusuri27 <jusuri27@gmail.com>
2026-03-14 09:54:32 +0900

    [Silver I] Title: 스티커, Time: 1584 ms, Memory: 320992 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -7,24 +7,38 @@ public class Main {
         int t = sc.nextInt();
         for(int i=0; i<t; i++) {
             int n = sc.nextInt();
-            int[][] arr = new int[2][n+1];
-            int[][] dp = new int[2][n+1];
-
-            for(int j=0; j<arr.length; j++) {
-                for(int k=1; k<arr[j].length; k++) {
-                    arr[j][k] = sc.nextInt();
+            int[][] arr = new int[n+1][2];
+            int[][] dp = new int[n+1][2];
+            for(int j=0; j<2; j++) {
+                for(int k=1; k<arr.length; k++) {
+                    arr[k][j] = sc.nextInt();
                 }
             }
 
-            dp[0][1] = arr[0][1];
+            dp[1][0] = arr[1][0];
             dp[1][1] = arr[1][1];
 
-            for(int k=2; k<=n; k++) {
-                dp[0][k] = Math.max(dp[1][k-1], dp[1][k-2]) + arr[0][k];
-                dp[1][k] = Math.max(dp[0][k-1], dp[0][k-2]) + arr[1][k];
+            for(int j=2; j<dp.length; j++) {
+                dp[j][0] = Math.max(dp[j-1][1], dp[j-2][1]) + arr[j][0];
+                dp[j][1] = Math.max(dp[j-1][0], dp[j-2][0]) + arr[j][1];
             }
 
-            System.out.println(Math.max(dp[0][n], dp[1][n]));
+            System.out.println(Math.max(dp[n][0], dp[n][1]));
         }
     }
 }
+
+/*
+dp[j][0] = Math.max(dp[j - 1][1], dp[j - 2][1]) + stickers[j][0];
+dp[j][1] = Math.max(dp[j - 1][0], dp[j - 2][0]) + stickers[j][1];
+
+
+0 50 40  200 140 250
+0 30 100 120 210 200
+
+0 50 10 100 20 40
+0 30 50 70 10 60
+
+
+
+ */
