da794a4a20998a5d73e5776087c7d72cdf081dcc
jusuri27 <jusuri27@gmail.com>
2026-03-14 16:11:01 +0900

    [Gold IV] Title: 동전 1, Time: 188 ms, Memory: 17904 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -5,18 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        int[] arr = new int[n];
+        int[] arr = new int[n+1];
         int[] dp = new int[k+1];
-
-        for(int i=0; i<arr.length; i++) {
+        for(int i=1; i<arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-
         dp[0] = 1;
-        for(int i=0; i<arr.length; i++) {
+        for(int i=1; i<arr.length; i++) {
             for(int j=1; j<dp.length; j++) {
                 if(j >= arr[i]) {
-                    dp[j] = dp[j] + dp[j-arr[i]];
+                    dp[j] = dp[j] + dp[j - arr[i]];
                 }
             }
         }
