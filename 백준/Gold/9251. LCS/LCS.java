4863c310c6d9428d255ba62eeec01b4c52710bb9
jusuri27 <jusuri27@gmail.com>
2026-03-15 20:49:28 +0900

    [Gold IV] Title: LCS, Time: 204 ms, Memory: 21996 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,7 +6,6 @@ public class Main {
         String str1 = sc.next();
         String str2 = sc.next();
         int[][] dp = new int[str1.length()+1][str2.length()+1];
-
         for(int i=1; i<dp.length; i++) {
             for(int j=1; j<dp[i].length; j++) {
                 if(str1.charAt(i-1) == str2.charAt(j-1)) {
@@ -20,12 +18,22 @@ public class Main {
         System.out.println(dp[str1.length()][str2.length()]);
     }
 }
+
+
 /*
   A C A Y K P
 C 0 1 1 1 1 1
 A 1 1 2 2 2 2
-P
-C
-A
-K
+P 1 1 2 2 2 3
+C 1 2 2 2 2 3
+A 2 2 3 3 3 3
+K 2 2 3 3 4 4
+
+  A C A
+C 0 1 0
+A 1 1 2
+P 1 1 2
+C 1 2 2
+A 2 2 3
+K 2 2 3
  */
