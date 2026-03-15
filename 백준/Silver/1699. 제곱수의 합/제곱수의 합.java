283ad09def54cc666d58fd48420ac924542f83cf
jusuri27 <jusuri27@gmail.com>
2026-03-15 20:23:17 +0900

    [Silver II] Title: 제곱수의 합, Time: 224 ms, Memory: 18336 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,20 +7,26 @@ public class Main {
         int n = sc.nextInt();
         int[] dp = new int[n+1];
 
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<dp.length; i++) {
             dp[i] = Integer.MAX_VALUE;
             for(int j=1; j*j<=i; j++) {
-                dp[i] = Math.min(dp[i], dp[i - (j*j)] + 1);
+                int value = j*j;
+                dp[i] = Math.min(dp[i], dp[i-value] + 1);
             }
         }
         System.out.println(dp[n]);
     }
 }
 
-
 /*
-dp의 낮은 값 순서대로 최소개수 경우의수를 저장한다
-dp[15] = dp[15 - (3*3)] + 1
-dp[15] = dp[15 - (2*2)] + 1
-dp[15] = dp[15 - (1*1)] + 1
+0 0
+1 1
+2 1 1
+3 1 1 1
+4 2
+5 2 1
+6 2 1 1
+7 2 1 1 1
+8 2 2
+9 3
  */
