9510fbea57ce28791c21375b048420d293242460
jusuri27 <jusuri27@gmail.com>
2026-03-16 11:48:38 +0900

    [Silver I] Title: 그림, Time: 1088 ms, Memory: 228252 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int[][] arr;
+    static boolean[][] visited;
+    static int max = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        arr = new int[n][m];
+        visited = new boolean[n][m];
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[i].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int count = 0;
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[i].length; j++) {
+                if(arr[i][j] == 1 && !visited[i][j]) {
+                    count++;
+                    bfs(i, j);
+                }
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(max);
+    }
+
+    public static void bfs(int x, int y) {
+        visited[x][y] = true;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y});
+
+        int count = 1;
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int cx = current[0];
+            int cy = current[1];
+
+            for(int i=0; i<dx.length; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx >= 0 && ny >= 0 && nx < arr.length && ny <arr[0].length) {
+                    if(arr[nx][ny] == 1 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        count++;
+                        queue.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+        max = Math.max(max, count);
+    }
+}
