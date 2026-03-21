2d2e2e7daaec8a7f3aaa7679d0ef8839868ce7ac
jusuri27 <jusuri27@gmail.com>
2026-03-21 12:01:33 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 2488 ms, Memory: 258128 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Main {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static boolean[][] visited;
     static int[][] arr;
+    static boolean[][] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -16,14 +16,13 @@ public class Main {
         visited = new boolean[n][m];
 
         for(int i=0; i<arr.length; i++) {
-            for(int j=0; j<arr[i].length; j++){
+            for(int j=0; j<arr[i].length; j++) {
                 int value = sc.nextInt();
                 if(value == 1) {
                     arr[i][j] = -1;
                 } else {
                     arr[i][j] = value;
                 }
-
             }
         }
 
@@ -44,10 +43,10 @@ public class Main {
     }
 
     public static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y, 0});
         visited[x][y] = true;
         arr[x][y] = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x, y, 1});
 
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -58,10 +57,11 @@ public class Main {
             for(int i=0; i<dx.length; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
+
                 if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length) {
                     if(arr[nx][ny] == -1 && !visited[nx][ny]) {
                         visited[nx][ny] = true;
-                        arr[nx][ny] = cnt + 1;
+                        arr[nx][ny] = cnt;
                         queue.add(new int[]{nx, ny, cnt+1});
                     }
                 }
