534b1d1037bbf636f068155456e533ff77632ae9
jusuri27 <jusuri27@gmail.com>
2026-03-21 11:27:40 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 852 ms, Memory: 153728 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,65 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    static int[][] arr;
-    static boolean[][][] visited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+    static int[][] arr;
+    static boolean[][][] visited;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
         arr = new int[n][m];
         visited = new boolean[n][m][2];
 
         for(int i=0; i<arr.length; i++) {
-            String str = br.readLine();
+            String str = sc.next();
             for(int j=0; j<str.length(); j++) {
                 arr[i][j] = str.charAt(j) - '0';
             }
         }
-        System.out.println(bfs(n, m));
-    }
 
-    public static int bfs(int n, int m) {
+        bfs(0,0);
+    }
+    public static void bfs(int x, int y) {
+        visited[x][y][0] = true;
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0, 0, 1, 0});
-        visited[0][0][0] = true;
+        queue.add(new int[]{x, y, 0, 1});
 
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
             int cx = current[0];
             int cy = current[1];
-            int cnt = current[2];
-            int broken = current[3];
+            int broken = current[2];
+            int cnt = current[3];
 
-            if(cx == n - 1 && cy == m - 1) {
-                return cnt;
+            if(cx == arr.length-1 && cy == arr[0].length -1) {
+                System.out.println(cnt);
+                return;
             }
 
-            for(int i=0; i<dx.length; i++) {
+
+            for (int i = 0; i < dx.length; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if(arr[nx][ny] == 0 && !visited[nx][ny][broken]) {
+                if (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length) {
+
+                    // 통과 조건
+                    if (arr[nx][ny] == 0 && !visited[nx][ny][broken]) {
                         visited[nx][ny][broken] = true;
-                        queue.add(new int[]{nx, ny, cnt + 1, broken});
+                        queue.add(new int[]{nx, ny, broken, cnt + 1});
                     }
 
-                    if(arr[nx][ny] == 1 && broken == 0 && !visited[nx][ny][1]) {
+                    // 한번 부술수 있는 조건
+                    if (arr[nx][ny] == 1 && !visited[nx][ny][1] && broken == 0) {
                         visited[nx][ny][1] = true;
-                        queue.add(new int[]{nx, ny, cnt + 1, 1});
+                        queue.add(new int[]{nx, ny, 1, cnt + 1});
                     }
                 }
             }
         }
-        return -1;
+        System.out.println(-1);
     }
 }
