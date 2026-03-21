2e1af3215ba26614aa83155a90b2374734d77316
jusuri27 <jusuri27@gmail.com>
2026-03-21 12:25:17 +0900

    [Silver I] Title: 스타트링크, Time: 264 ms, Memory: 65512 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,13 +1,13 @@
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    static int[] move;
-    static boolean[] visited;
-    static int f;
+    static int[] move = new int[2];
     static int g;
+    static int f;
+
+    static boolean[] visited;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         f = sc.nextInt();
@@ -15,43 +15,45 @@ public class Main {
         g = sc.nextInt();
         int u = sc.nextInt();
         int d = sc.nextInt();
-        move = new int[2];
-
         move[0] = u;
         move[1] = -d;
         visited = new boolean[f+1];
+
         bfs(s);
     }
 
     public static void bfs(int start) {
-        int count = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{start, count});
         visited[start] = true;
-        while(!queue.isEmpty()) {
-            int[] value = queue.poll();
-            int current = value[0];
-            int cnt = value[1];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, 0});
 
-            if(current == g) {
+        while(!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int now = current[0];
+            int cnt = current[1];
+
+            if(now == g) {
                 System.out.println(cnt);
                 return;
             }
 
             for(int i=0; i<move.length; i++) {
-                int next = current + move[i];
-                if(next <= f && next >= 1 && !visited[next]) {
+                int next = now + move[i];
+                if(next > 0 && next <= f && !visited[next]){
                     visited[next] = true;
-                    queue.add(new int[]{next, cnt+1});
+                    queue.add(new int[]{next, cnt + 1});
                 }
             }
         }
         System.out.println("use the stairs");
     }
 }
-// 3 5 7 9
-// f : 최고층
-// s : 현재층
-// g : 타겟층
-// u : 위로
-// d : 아래로
+
+/*
+1 3 5 7 9 8 10
+총층 : f
+현재층 : s
+타겟층 : g
+위로 : u
+아래로 : d
+ */
