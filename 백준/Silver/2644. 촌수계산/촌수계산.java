60466dad5f72240bd8b26ca505bb7ba2563154a3
jusuri27 <jusuri27@gmail.com>
2026-03-22 20:13:58 +0900

    [Silver II] Title: 촌수계산, Time: 184 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -2,15 +2,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    static int end;
     static int[][] arr;
     static boolean[] visited;
-    static int num2;
-    static int answer = -1;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int num1 = sc.nextInt();
-        num2 = sc.nextInt();
+        int start = sc.nextInt();
+        end = sc.nextInt();
         int m = sc.nextInt();
         arr = new int[n+1][n+1];
         visited = new boolean[n+1];
@@ -23,27 +22,22 @@ public class Main {
             arr[y][x] = 1;
         }
 
-        dfs(num1, 0);
-        System.out.println(answer);
+        dfs(start, 0);
+        System.out.println(-1);
     }
-    public static void dfs(int start, int count) {
+
+    public static void dfs(int start, int depth) {
         visited[start] = true;
-        if(start == num2) {
-            answer = count;
-            return;
+        if(start == end) {
+            System.out.println(depth);
+            System.exit(0);
         }
-        for(int j=0; j<arr[0].length; j++) {
-            if(arr[start][j] == 1 && !visited[j]) {
-                visited[j] = true;
-                dfs(j, count + 1);
+
+
+        for(int i=0; i<arr[0].length; i++) {
+            if(arr[start][i] == 1 && !visited[i]) {
+                dfs(i, depth + 1);
             }
         }
     }
 }
-
-/*
-첫째 줄 : 전체 사람의 수 n
-둘째 줄 : 촌수를 계산해야하는 서로 다른 두 사람의 번호 ex) 7 3
-셋째 줄 : 부모 자식들 간의 관계의 개수 m
-넷째 줄 : 부모 : x, 자식 : y
- */
