7fd177f75243f67c777e9ddf6e1abf69a6843f94
jusuri27 <jusuri27@gmail.com>
2026-03-28 10:10:13 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 180 ms, Memory: 17760 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,27 +1,23 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-
         int count = 0;
         for(int i=0; i<n; i++) {
-            HashMap<Character, Integer> map = new HashMap<>();
             String str = sc.nextLine();
+            char[] ch = new char[26];
             boolean check = true;
-            map.put(str.charAt(0), 0);
+            ch[str.charAt(0)-'a']++;
             for(int j=1; j<str.length(); j++) {
-                Character value = str.charAt(j);
-                if(map.containsKey(value)) {
-                    if(map.get(value) != j-1) {
-                        check = false;
-                        break;
-                    }
+                int current = str.charAt(j) - 'a';
+                int prev = str.charAt(j-1) - 'a';
+                if(prev != current && ch[current] != 0) {
+                    check = false;
+                    break;
                 }
-                map.put(value, j);
+                ch[current]++;
             }
             if(check) {
                 count++;
