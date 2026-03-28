bedb044747544c97c4d870ae0ec4a610dff609f2
jusuri27 <jusuri27@gmail.com>
2026-03-28 10:55:22 +0900

    [Gold IV] Title: 알파벳, Time: 972 ms, Memory: 15140 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,22 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static char[][] arr;
     static boolean[] visited = new boolean[26];
+    static char[][] arr;
     static int max = 0;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         arr = new char[n][m];
 
         for(int i=0; i<arr.length; i++) {
-            String str = sc.next();
+            String str = br.readLine();
             for(int j=0; j<str.length(); j++) {
-                arr[i][j] = str.charAt(j);
+                char value = str.charAt(j);
+                arr[i][j] = value;
             }
         }
         visited[arr[0][0] - 'A'] = true;
@@ -30,13 +35,12 @@ public class Main {
         for(int i=0; i<dx.length; i++) {
             int nx = cx + dx[i];
             int ny = cy + dy[i];
-
             if(nx >= 0 && ny >= 0 && nx < arr.length && ny < arr[0].length) {
-                int idx = arr[nx][ny] - 'A';
-                if(!visited[idx]) {
-                    visited[idx] = true;
-                    dfs(nx, ny, cnt +1);
-                    visited[idx] = false;
+                int current = arr[nx][ny] - 'A';
+                if(!visited[current]) {
+                    visited[current] = true;
+                    dfs(nx, ny, cnt + 1);
+                    visited[current] = false;
                 }
             }
         }
