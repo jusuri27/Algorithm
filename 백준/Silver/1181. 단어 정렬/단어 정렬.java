4e77f39f9dda110826069d74467de00c5a343894
jusuri27 <jusuri27@gmail.com>
2026-03-29 17:19:56 +0900

    [Silver V] Title: 단어 정렬, Time: 808 ms, Memory: 67520 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
-        Set<String > set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
 
         for(int i=0; i<n; i++) {
             set.add(sc.nextLine());
         }
-
         List<String> list = new ArrayList<>(set);
+
         list.sort((a, b) -> {
             if(a.length() == b.length()) {
                 return a.compareTo(b);
@@ -18,8 +18,8 @@ public class Main {
             return a.length() - b.length();
         });
 
-        for(int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
+        for(String str : list) {
+            System.out.println(str);
         }
     }
 }
