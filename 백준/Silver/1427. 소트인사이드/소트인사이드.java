b94bc6170d4bfbf5547bf5b4c5d5d7acc1613385
jusuri27 <jusuri27@gmail.com>
2026-03-29 18:01:01 +0900

    [Silver V] Title: 소트인사이드, Time: 180 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,14 +1,13 @@
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr = sc.nextLine().split("");
-        Arrays.sort(arr, Comparator.reverseOrder());
-        for(String value : arr) {
-            System.out.print(value);
+        char[] ch = sc.nextLine().toCharArray();
+        Arrays.sort(ch);
+        for(int i=ch.length-1; i>=0; i--) {
+            System.out.print(ch[i]);
         }
     }
 }
