580034618f856832b4c6cea974c96cee88b11cbe
jusuri27 <jusuri27@gmail.com>
2026-03-29 17:28:37 +0900

    [Silver IV] Title: 베스트셀러, Time: 184 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,4 +1,7 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,20 +9,19 @@ public class Main {
         int n = Integer.parseInt(sc.nextLine());
         HashMap<String, Integer> map = new HashMap<>();
 
-        int max = 0;
-        for(int i=0; i<n; i++) {
+        for(int i=0;  i<n; i++) {
             String value = sc.nextLine();
-            map.put(value, map.getOrDefault(value, 1) + 1);
-            max = Math.max(max, map.get(value));
+            map.put(value, map.getOrDefault(value, 0) + 1);
         }
 
-        List<String> list = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            if(entry.getValue() == max) {
-                list.add(entry.getKey());
-            }
-        }
-        list.sort(null);
+        List<String> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> {
+           if(map.get(a) == map.get(b)) {
+               return a.compareTo(b);
+           }
+           return map.get(b) - map.get(a);
+        });
+
         System.out.println(list.get(0));
     }
 }
