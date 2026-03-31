d68c6536f8d204933e11eccc9f1d228108cd2399
jusuri27 <jusuri27@gmail.com>
2026-03-31 12:05:06 +0900

    [Silver V] Title: 생일, Time: 176 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String[][] arr = new String[n][4];
+
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = sc.nextLine().split(" ");
+        }
+
+        Arrays.sort(arr, (a,b) -> {
+            int yearA = Integer.parseInt(a[3]);
+            int yearB = Integer.parseInt(b[3]);
+            int monthA = Integer.parseInt(a[2]);
+            int monthB = Integer.parseInt(b[2]);
+            int dayA = Integer.parseInt(a[1]);
+            int dayB = Integer.parseInt(b[1]);
+
+            if(yearA == yearB && monthA == monthB) {
+                return dayA - dayB;
+            }
+
+            if(yearA == yearB) {
+                return monthA - monthB;
+            }
+
+            return yearA - yearB;
+        });
+
+        System.out.println(arr[n-1][0]);
+        System.out.println(arr[0][0]);
+    }
+}
