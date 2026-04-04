c2a767bd3d14b9f1c987355ce71b78160aa5e17b
jusuri27 <jusuri27@gmail.com>
2026-04-04 10:12:31 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 796 ms, Memory: 76112 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = Integer.parseInt(sc.nextLine());
+
         for(int i=0; i<n; i++) {
-            String[] arr = sc.nextLine().split(" ");
+            String[] str = sc.nextLine().split(" ");
             StringBuilder sb = new StringBuilder();
-            for(int j=0; j<arr.length; j++) {
-                String str = new StringBuilder(arr[j]).reverse().toString();
-                sb.append(str + " ");
+            for(int j=0; j<str.length; j++) {
+                String reverseStr = new StringBuilder(str[j]).reverse().toString();
+                sb.append(reverseStr + " ");
             }
             System.out.println(sb);
         }
