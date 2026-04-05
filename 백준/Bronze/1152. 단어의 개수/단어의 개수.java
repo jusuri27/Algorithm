69969302778370d9a6f3fe19c666a7e9fabca6ea
jusuri27 <jusuri27@gmail.com>
2026-04-05 22:44:37 +0900

    [Bronze II] Title: 단어의 개수, Time: 492 ms, Memory: 62056 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr = sc.nextLine().split(" ");
-        int result = 0;
-        for(int i=0; i<arr.length; i++) {
-            if(!arr[i].isEmpty()) {
-                result++;
+        String[] str = sc.nextLine().split(" ");
+
+        int count = 0;
+        for(int i=0; i<str.length; i++) {
+            if(!str[i].isEmpty()) {
+                count++;
             }
         }
-        System.out.println(result);
+        System.out.println(count);
     }
 }
