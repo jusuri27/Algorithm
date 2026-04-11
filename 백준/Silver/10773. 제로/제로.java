45ced3edb20e58d6c0a80092b17d702c00a39bcb
jusuri27 <jusuri27@gmail.com>
2026-04-11 09:50:06 +0900

    [Silver IV] Title: 제로, Time: 708 ms, Memory: 110424 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,12 +1,12 @@
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Deque<Integer> stack = new ArrayDeque<>();
+        int answer = 0;
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
         for(int i=0; i<n; i++) {
             int value = sc.nextInt();
@@ -17,7 +17,6 @@ public class Main {
             }
         }
 
-        int answer = 0;
         for(int i : stack) {
             answer += i;
         }
