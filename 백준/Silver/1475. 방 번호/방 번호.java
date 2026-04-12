fa5910ae72b14c80da0108afbac8be1f89d29a26
jusuri27 <jusuri27@gmail.com>
2026-04-12 21:59:08 +0900

    [Silver V] Title: 방 번호, Time: 168 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+        String str = String.valueOf(sc.nextInt());
         int[] arr = new int[10];
 
         for(int i=0; i<str.length(); i++) {
@@ -14,13 +15,14 @@ public class Main {
                 arr[value]++;
             }
         }
-        arr[6] = (int) Math.round(arr[6] / 2.0);
 
+        arr[6] = (arr[6] + 1) / 2;
         int answer = 0;
         for(int i : arr) {
             answer = Math.max(answer, i);
         }
 
         System.out.println(answer);
+
     }
 }
