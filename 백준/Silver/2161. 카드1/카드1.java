43d4a4de6f158a992a5b6f68768eba152268df96
jusuri27 <jusuri27@gmail.com>
2026-04-13 09:47:58 +0900

    [Silver V] Title: 카드1, Time: 204 ms, Memory: 18088 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.java
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -1,11 +1,12 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Queue<Integer> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
 
         for(int i=1; i<=n; i++) {
             queue.add(i);
@@ -13,14 +14,11 @@ public class Main {
 
         while(queue.size() != 1) {
             Integer removed = queue.poll();
-            list.add(removed);
             Integer switched = queue.poll();
             queue.add(switched);
-        }
-        list.add(queue.poll());
 
-        for(int i : list) {
-            System.out.print(i + " ");
+            System.out.print(removed + " ");
         }
+        System.out.println(queue.poll());
     }
 }
