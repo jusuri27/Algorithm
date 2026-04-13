cfad2ad0ac65ddd854f70ae56f14dc0f56809e7f
jusuri27 <jusuri27@gmail.com>
2026-04-13 10:25:34 +0900

    [Bronze III] Title: 최소, 최대, Time: 1552 ms, Memory: 311992 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<n; i++) {
+            int value = sc.nextInt();
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+        }
+        System.out.println(min + " " + max);
+    }
+}
