6ac8ca1dea72da2d117705d4cb7a54cd33f9b214
jusuri27 <jusuri27@gmail.com>
2026-04-15 10:09:44 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,36 +6,33 @@ public class Main {
         int n = sc.nextInt();
         int k = sc.nextInt();
         boolean[] isPrime = new boolean[n+1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = true;
-        isPrime[1] = true;
 
         int count = 0;
         int answer = 0;
         boolean found = false;
         for(int i=2; i<=n; i++) {
-            if(isPrime[i]) {
+            if(!isPrime[i]) {
+                isPrime[i] = true;
                 count++;
-                for(int j=i*i; j<=n; j+=i) {
-                    if(isPrime[j]) {
-                        isPrime[j] = false;
+                if(count == k) {
+                    answer = i;
+                    break;
+                }
+                for(int j=i; j<=n; j+=i) {
+                    if(!isPrime[j]) {
+                        isPrime[j] = true;
                         count++;
-                    }
-                    if(count == k) {
-                        found = true;
-                        answer = j;
-                        break;
+                        if(count == k) {
+                            found = true;
+                            answer = j;
+                            break;
+                        }
                     }
                 }
             }
             if(found) {
                 break;
             }
-            if(count == k) {
-                answer = i;
-                break;
-            }
-
         }
         System.out.println(answer);
     }
