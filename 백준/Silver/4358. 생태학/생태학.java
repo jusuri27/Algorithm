b9db1a87d63e4e9a5584388f738d731b84ab1b4f
jusuri27 <jusuri27@gmail.com>
2026-04-17 10:19:20 +0900

    [Silver II] Title: 생태학, Time: 1868 ms, Memory: 309292 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,13 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Map<String, Integer> map = new TreeMap<>();
         int size = 0;
@@ -19,10 +16,9 @@ public class Main {
             size++;
             map.put(value, map.getOrDefault(value, 0) + 1);
         }
+
         for(Map.Entry<String, Integer> entry : map.entrySet()) {
             double value = (double) (entry.getValue() * 100) / size;
-//            double percent = Math.round(value * 10000) / 10000.0;
-//            System.out.println(entry.getKey() + " " + percent);
             System.out.printf("%s %.4f\n", entry.getKey(), value);
         }
     }
