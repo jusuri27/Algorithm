fd4f6a13b99ad9a0f847691f453adead66eb8c9d
jusuri27 <jusuri27@gmail.com>
2026-04-18 17:19:29 +0900

    [Bronze III] Title: 최소, 최대, Time: 1560 ms, Memory: 311552 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -6,7 +6,6 @@ public class Main {
         int n = sc.nextInt();
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-
         for(int i=0; i<n; i++) {
             int value = sc.nextInt();
             max = Math.max(max, value);
