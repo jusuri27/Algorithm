22ac72fea9da997d8e0166fd169da75061f29aab
jusuri27 <jusuri27@gmail.com>
2026-04-18 17:53:27 +0900

    [Silver II] Title: 비슷한 단어, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,10 +1,11 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        String baseWord = sc.nextLine();
+        int n = sc.nextInt();
+        String baseWord = sc.next();
         int[] baseCount = new int[26];
 
         for(int i=0; i<baseWord.length(); i++) {
@@ -13,25 +14,22 @@ public class Main {
 
         int answer = 0;
         for(int i=1; i<n; i++) {
-            String currentWord = sc.nextLine();
+            String currentWord = sc.next();
             int[] currentCount = new int[26];
             for(int j=0; j<currentWord.length(); j++) {
                 currentCount[currentWord.charAt(j) - 'A']++;
             }
 
             int diff = 0;
-            for(int k=0; k<baseCount.length; k++) {
+            for(int k=0; k<currentCount.length; k++) {
                 diff += Math.abs(baseCount[k] - currentCount[k]);
             }
-
-
             if(diff <= 1) {
                 answer++;
             } else if(diff == 2 && baseWord.length() == currentWord.length()) {
                 answer++;
             }
         }
-
         System.out.println(answer);
     }
 }
