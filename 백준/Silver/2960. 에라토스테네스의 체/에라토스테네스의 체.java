793cc871dbac45686214732ca09b70cd202fc4c9
jusuri27 <jusuri27@gmail.com>
2026-04-19 17:16:15 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 168 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -11,22 +11,14 @@ public class Main {
         int answer = 0;
         boolean found = false;
         for(int i=2; i<=n; i++) {
-            if(!isPrime[i]) {
-                isPrime[i] = true;
-                count++;
-                if(count == k) {
-                    answer = i;
-                    break;
-                }
-                for(int j=i; j<=n; j+=i) {
-                    if(!isPrime[j]) {
-                        isPrime[j] = true;
-                        count++;
-                        if(count == k) {
-                            found = true;
-                            answer = j;
-                            break;
-                        }
+            for(int j=i; j<=n; j+=i) {
+                if(!isPrime[j]) {
+                    isPrime[j] = true;
+                    count++;
+                    if(count == k) {
+                        answer = j;
+                        found = true;
+                        break;
                     }
                 }
             }
