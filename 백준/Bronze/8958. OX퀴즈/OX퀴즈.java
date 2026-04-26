e2c185057745137b119630187c20effe808d69eb
jusuri27 <jusuri27@gmail.com>
2026-04-26 22:07:18 +0900

    [Bronze II] Title: OX퀴즈, Time: 184 ms, Memory: 17856 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -4,13 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         for(int i=0; i<n; i++) {
             String str = sc.next();
-            int count = 0;
+            char current = str.charAt(0);
             int sum = 0;
-            for(int j=0; j<str.length(); j++) {
-                if(str.charAt(j) == 'O') {
+            int count = 0;
+            if(current == 'O') {
+                count++;
+                sum += count;
+            }
+            for(int j=1; j<str.length(); j++) {
+                current = str.charAt(j);
+                if(current == 'O') {
                     count++;
                     sum += count;
                 } else {
