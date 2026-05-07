8836d67e26919d56b04926a63a00513a82bc2cfa
jusuri27 <jusuri27@gmail.com>
2026-05-07 20:21:47 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "Even";
+        if(num % 2 == 1 || num % 2 == -1) {
+            answer = "Odd";
+        }
+        
+        return answer;
+    }
+}
