4e75efb6c8a79371ffed21b9f462143a1f409327
jusuri27 <jusuri27@gmail.com>
2026-05-09 12:24:09 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,10 @@
-import java.util.*;
-
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        String[] str = String.valueOf(x).split("");
-        
+        String str = String.valueOf(x);
         int sum = 0;
-        for(int i=0; i<str.length; i++) {
-            sum += Integer.parseInt(str[i]);
+        for(int i=0; i<str.length(); i++) {
+            sum += str.charAt(i) - '0';
         }
         
         if(x % sum != 0) {
