2ca85a428d23ad7524cdf0627cd2276c97622105
jusuri27 <jusuri27@gmail.com>
2026-05-17 21:32:44 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        for(int i=1; i<=9; i++) {
+        for(int i=0; i<=9; i++) {
             boolean found = false;
             for(int j=0; j<numbers.length; j++) {
                 if(i == numbers[j]) {
