e9ae2c07efa94f4f69c7ba93839d8dfdcc9f87bf
jusuri27 <jusuri27@gmail.com>
2026-05-18 18:14:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int num) {
+        int answer = -1;
+        if(num == 1) {
+            answer = 0;
+            return answer;
+        }
+        
+        for(int i=1; i<= 500; i++) {
+            if(num % 2 == 1) {
+                num *= 3;
+                num += 1;
+            } else {
+                num /= 2;
+            }
+
+            if(num == 1) {
+                answer = i;
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
+}
