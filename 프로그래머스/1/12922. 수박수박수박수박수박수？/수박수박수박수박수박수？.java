a1e4ab8eae52a5ffe4fe0b929a41e662bb090fe8
jusuri27 <jusuri27@gmail.com>
2026-05-30 18:01:33 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 37.70 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<n; i++) {
             if(i % 2 == 1) {
-                answer += "수";
-            } else {
                 answer += "박";
+            } else {
+                answer += "수";
             }
         }
         return answer;
