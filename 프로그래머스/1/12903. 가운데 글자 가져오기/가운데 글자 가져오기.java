d234beeb024a615ec31352a300540a316ef704c5
jusuri27 <jusuri27@gmail.com>
2026-05-30 17:58:37 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -3,9 +3,9 @@ class Solution {
         String answer = "";
         int mid = s.length() / 2;
         if(s.length() % 2 == 1) {
-            answer = String.valueOf(s.charAt(mid));
+            answer = s.substring(mid, mid+1);
         } else {
-            answer = s.substring(mid - 1, mid + 1);
+            answer = s.substring(mid-1, mid+1);
         }
         return answer;
     }
