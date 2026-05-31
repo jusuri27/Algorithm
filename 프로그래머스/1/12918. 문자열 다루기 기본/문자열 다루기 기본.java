6b08e1ac4baf24e4eb2e33e55bc07782d073d162
jusuri27 <jusuri27@gmail.com>
2026-05-31 19:47:54 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
-        if(s.length() == 4 || s.length() == 6) {
-            for(int i=0; i<s.length(); i++) {
-                if(!Character.isDigit(s.charAt(i))) {
-                    return false;
-                } else {
-                    answer = true;
-                }
+        boolean answer = true;
+        if(s.length() != 4 && s.length() != 6) {
+            answer = false;
+        }
+        
+        for(int i=0; i<s.length(); i++) {
+            char value = s.charAt(i);
+            if(!Character.isDigit(value)) {
+                answer = false;
             }
         }
         return answer;
