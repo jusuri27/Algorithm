b9307eb03dd02e3ebd7930b022622d47ccf04e23
jusuri27 <jusuri27@gmail.com>
2026-06-02 21:25:45 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 4.36 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int count = 0;
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == ' ') {
+                answer += ' ';
+                count = 0;
+            } else {
+                count++;
+                if(count % 2 == 1) {
+                    answer += Character.toUpperCase(ch);
+                } else {
+                    answer += Character.toLowerCase(ch);;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
