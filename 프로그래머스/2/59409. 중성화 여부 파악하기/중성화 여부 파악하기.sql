8a34e9e436c855e9b2fd3719f2e8f8691bf728f3
jusuri27 <jusuri27@gmail.com>
2026-07-27 11:40:57 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME,
+    case
+        when SEX_UPON_INTAKE like '%Neutered%' or SEX_UPON_INTAKE like '%Spayed%' then 'O'
+        else 'X'
+    end
+from
+    ANIMAL_INS 
